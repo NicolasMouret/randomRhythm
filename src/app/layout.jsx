3be3a5cb51ac8a54f02abe6c4206e6423ac1f701
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-08 14:40:59 +0100

    update favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,12 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <MetronomeProvider>
           <body className={josefin.className}>
           <h1 className="mt-10 text-center text-7xl text-slate-800">RandomRhythm</h1>
