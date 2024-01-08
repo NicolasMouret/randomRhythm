7c64c2db8d9749d47a9dce5b804727ce82ac06bc
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-08 14:17:14 +0100

    update favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
-import { Josefin_Sans } from 'next/font/google'
 import { MetronomeProvider } from '@/contexts/MetronomeContext';
-import './globals.css'
+import { Josefin_Sans } from 'next/font/google';
+import './globals.css';
 
 const josefin = Josefin_Sans({ subsets: ['latin'] })
 
@@ -13,6 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <MetronomeProvider>
           <body className={josefin.className}>
           <h1 className="mt-10 text-center text-7xl text-slate-800">RandomRhythm</h1>
